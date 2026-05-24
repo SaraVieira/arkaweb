273423d42bbd282d9cc8e8637e6fad4ba427c8cb
SaraVieira <hey@iamsaravieira.com>
2026-05-24 11:13:18 +0100

    improver settings look

--- src/components/overlay.tsx
+++ src/components/overlay.tsx
@@ -26,23 +26,43 @@ export const Overlay = ({
           {actionLabel}
         </button>
       )}
-      <div>
+
+      <h3 className="text-4xl font-bold tracking-widest">Effects</h3>
+      <div className="flex items-center gap-4">
         {Object.entries(settings).map(([key, value]) => {
           const settingKey = key as settingsEnum;
           return (
-            <label key={key} className="mr-4">
+            <label
+              key={key}
+              className="flex gap-1 items-center cursor-pointer relative"
+            >
               <input
-                type="checkbox"
-                checked={value}
                 onChange={() =>
                   setSettings((prev) => ({
                     ...prev,
                     [settingKey]: !prev[settingKey],
                   }))
                 }
-                className="mr-1"
+                checked={value}
+                type="checkbox"
+                className="hidden peer"
               />
-              {key}
+              <span className="w-5 h-5 border border-slate-300 rounded relative flex items-center justify-center peer-checked:border-green-300"></span>
+              <svg
+                className="absolute hidden peer-checked:inline left-1 top-1/2 transform -translate-y-1/2"
+                width="11"
+                height="8"
+                viewBox="0 0 11 8"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  className="fill-green-200 stroke-green-200"
+                  d="m10.092.952-.005-.006-.006-.005A.45.45 0 0 0 9.43.939L4.162 6.23 1.585 3.636a.45.45 0 0 0-.652 0 .47.47 0 0 0 0 .657l.002.002L3.58 6.958a.8.8 0 0 0 .567.242.78.78 0 0 0 .567-.242l5.333-5.356a.474.474 0 0 0 .044-.65Zm-5.86 5.349V6.3Z"
+                  stroke-width=".4"
+                />
+              </svg>
+              <span className="text-gray-200 select-none">{key}</span>
             </label>
           );
         })}
